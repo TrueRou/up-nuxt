6b6757cbe2ec668ed143111bc720f1031846b806
TuRou <turou@email.swu.edu.cn>
2025-10-13 17:56:35 +0800

    fix: common response schema

--- app/def/common.ts
+++ app/def/common.ts
@@ -1,11 +1,6 @@
-/**
- * 通用响应体
- */
 export interface CommonResponse<T = any> {
-    /** 响应码 */
     code: number
-    /** 响应消息 */
-    message: string
-    /** 响应数据 */
+    node: string
+    message: string | null
     data: T | null
 }
